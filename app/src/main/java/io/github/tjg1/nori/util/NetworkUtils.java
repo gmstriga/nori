cf1b4ddf1179be59fb92c0c2a66102a7b42a1083
Tomasz Jan Góralczyk <tomg@fastmail.uk>
2016-08-06 11:04:17 +0100

    Update list of fast network types

--- app/src/main/java/io/github/tjg1/nori/util/NetworkUtils.java
+++ app/src/main/java/io/github/tjg1/nori/util/NetworkUtils.java
@@ -69,21 +69,16 @@ public abstract class NetworkUtils {
       // WiFi is good.
       return true;
     } else if (type == ConnectivityManager.TYPE_MOBILE) {
-      // Exclude mobile network types with avg speeds below or close to ~1Mbps.
-      // Giving new technologies introduced after this code was written benefit of the doubt.
+      // Exclude mobile network types with avg speeds below or close to ~5Mbps.
       switch (subType) {
-        case TelephonyManager.NETWORK_TYPE_1xRTT: // ~ 50-100 kbps
-        case TelephonyManager.NETWORK_TYPE_CDMA: // ~ 14-64 kbps
-        case TelephonyManager.NETWORK_TYPE_EDGE: // ~ 50-100 kbps
-        case TelephonyManager.NETWORK_TYPE_EVDO_0: // ~ 400-1000 kbps
-        case TelephonyManager.NETWORK_TYPE_EVDO_A: // ~ 600-1400 kbps
-        case TelephonyManager.NETWORK_TYPE_GPRS: // ~ 100 kbps
-        case TelephonyManager.NETWORK_TYPE_HSPA: // ~ 700-1700 kbps
-        case TelephonyManager.NETWORK_TYPE_IDEN: // ~ 25 kbps
-        case TelephonyManager.NETWORK_TYPE_EHRPD: // ~ 1-2 Mbps
-          return false;
-        default:
+        case TelephonyManager.NETWORK_TYPE_HSDPA: // 2-14 Mbps
+        case TelephonyManager.NETWORK_TYPE_HSUPA: // 1-23 Mbps
+        case TelephonyManager.NETWORK_TYPE_EVDO_B: // 5 Mbps
+        case TelephonyManager.NETWORK_TYPE_HSPAP: // 10-20 Mbps
+        case TelephonyManager.NETWORK_TYPE_LTE: // 10+ Mbps
           return true;
+        default:
+          return false;
       }
     } else {
       return false;
