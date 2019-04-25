c751bcb529c92329d70cc8a11d516f9703f51cd9
Tomasz Jan Góralczyk <tomg@fastmail.uk>
2019-04-25 23:31:56 +0100

    Fix Danbooru sample URLs
    YOU'D THINK a "large_file_url" would in fact be larger than a "file_url" @r888888888

--- norilib/src/main/java/io/github/tjg1/library/norilib/clients/Danbooru.java
+++ norilib/src/main/java/io/github/tjg1/library/norilib/clients/Danbooru.java
@@ -270,7 +270,7 @@ public class Danbooru implements SearchClient {
           }
           // Extract image metadata from XML tags.
           else if ("large-file-url".equals(name)) {
-            image.fileUrl = xpp.nextText();
+            image.sampleUrl = xpp.nextText();
           } else if ("image-width".equals(name)) {
             image.width = Integer.parseInt(xpp.nextText());
           } else if ("image-height".equals(name)) {
@@ -278,7 +278,7 @@ public class Danbooru implements SearchClient {
           } else if ("preview-file-url".equals(name)) {
             image.previewUrl = xpp.nextText();
           } else if ("file-url".equals(name)) {
-            image.sampleUrl = xpp.nextText();
+            image.fileUrl = xpp.nextText();
           } else if ("tag-string-general".equals(name)) {
             imageTags.addAll(Arrays.asList(Tag.arrayFromString(xpp.nextText(), Tag.Type.GENERAL)));
           } else if ("tag-string-artist".equals(name)) {
